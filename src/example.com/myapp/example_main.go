926bda2dcbf70b4ff39369364130c3600c36b7c0
James Graves <james.c.graves.jr@gmail.com>
2014-07-21 15:14:24 -0500

    Now using omicron and upsilon libs

--- src/example.com/myapp/example_main.go
+++ src/example.com/myapp/example_main.go
@@ -2,8 +2,12 @@ package main
 
 import (
 	"fmt"
+	"github.com/jamesgraves/omicron"
+	"github.com/upstream-author/upsilon"
 )
 
 func main() {
 	fmt.Println("This is main.")
+	omicron.Foo()
+	upsilon.Bar()
 }
